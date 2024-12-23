02b79477d4fbaea65ec27655f61a783e9e394bdc
dybucc <149513579+dybucc@users.noreply.github.com>
2024-12-23 09:51:19 +0100

    feat(juego): crea la eleccion de opciones en menu

--- main.cpp
+++ main.cpp
@@ -32,9 +32,37 @@ const unsigned short COL = 8;
 typedef Jugador VectorJ[MAX_JUGADORES];
 typedef Estado Tablero[FIL][COL];
 
+char Menu();
+
 int main(void)
 {
 
 
     return 0;
+}
+
+char Menu()
+{
+    char opcion = 'a';
+
+    do
+    {
+        if (opcion < 'a' || opcion > 'e')
+            cout << endl;
+
+        cout << "a. Lee las posiciones de las minas desde fichero."
+            << endl;
+        cout << "b. Genera aleatoriamente las posiciones de las minas."
+            << endl;
+        cout << "c. Descube celda." << endl;
+        cout << "d. Marca celda como una mina. Anade una bandera."
+            << endl;
+        cout << "e. Desmarca celda como una mina." << endl << endl;
+
+        cout << "> ";
+        cin >> opcion;
+    }
+    while (opcion < 'a' || opcion > 'e');
+
+    return opcion;
 }
